77e2694a20016f040eed815c190793efcd70332a
Takashi Horikawa <horikawa@nautilus-technologies.com>
2019-09-05 15:05:59 +0900

    Follow conventions for include description order

--- stub/mock/test/ApiTest.cpp
+++ stub/mock/test/ApiTest.cpp
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "gtest/gtest.h"
-
 #include "ogawayama/stub/api.h"
+
+#include "gtest/gtest.h"
 
 namespace ogawayama::testing {
 
