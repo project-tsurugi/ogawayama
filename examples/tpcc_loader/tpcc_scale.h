5232a7e6e2bac4944dc0c36bf7f11f159553f79d
Takashi Horikawa <horikawa@nautilus-technologies.com>
2019-09-19 12:22:56 +0900

    Enhance tpcc_loader to supprt scale parameter

--- examples/tpcc_loader/tpcc_scale.h
+++ examples/tpcc_loader/tpcc_scale.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019-2019 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef TPCC_SCALE_H_
 #define TPCC_SCALE_H_
 
@@ -8,51 +23,22 @@ namespace tpcc {
 
 struct Scale {
     /** Number of warehouses. Does not grow dynamically */
-    static constexpr inline std::uint16_t warehouses = 1;
+    std::uint16_t warehouses;
 
     /** Number of items per warehouse. Does not grow dynamically  */
-    static constexpr inline std::uint32_t items = 50;
+    std::uint32_t items;
 
     /** Number of districts per warehouse. Does not grow dynamically  */
-    static constexpr inline std::uint8_t districts = 2;
+    std::uint8_t districts;
 
     /** Number of customers per district. Does not grow dynamically  */
-    static constexpr inline std::uint32_t customers = 30;
+    std::uint32_t customers;
 
     /** Number of orders per district. Does grow dynamically. */
-    static constexpr inline std::uint32_t orders = 30;
-
-    /** Number of orderlines per order. Does not grow dynamically. */
-    static constexpr inline std::uint16_t max_ol_count = 6;
-    static constexpr inline std::uint8_t min_ol_count = 2;
-    static constexpr inline std::uint16_t max_ol = max_ol_count + 1U;
-
-    /** Number of variations of last names. Does not grow dynamically. */
-    static constexpr inline std::uint32_t lnames = 1000U;
-
-    static constexpr inline std::int32_t load_threads = 1;
+    std::uint32_t orders;
 };
 
-using scale = Scale;
-
-extern uint16_t warehouses;
-
-// See Sec 5.2.2 of the TPCC spec
-constexpr inline std::uint8_t kXctNewOrderPercent = 45U;
-constexpr inline std::uint8_t kXctPaymentPercent = 43U + kXctNewOrderPercent;
-constexpr inline std::uint8_t kXctOrderStatusPercent = 4U + kXctPaymentPercent;
-constexpr inline std::uint8_t kXctDelieveryPercent = 4U + kXctOrderStatusPercent;
-// remainings are stock-level xct.
-
-/**
- * How much of supplier in neworder transaction uses remote warehouse?
- * Note that one neworder xct touches on average 10 suppliers, so in total
- * a bit less than 10% of neworder accesses remote warehouse(s).
- */
-constexpr inline std::uint8_t kNewOrderRemotePercent = 1U;
-
-/** How much of payment transaction uses remote warehouse/district? */
-// const uint8_t kPaymentRemotePercent = 15U;
+extern struct Scale *scale, tiny, normal;
 
 }  // namespace tpcc
 }  // namespace ogawayama
