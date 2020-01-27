project-tsurugi/ogawayama

commit d63cbf62ae0d6840892442c671aedc2eb55a31b5

Fix an issue caused by result sets being stored on the application side

server/src/backend.cpp
@@ -81,7 +81,6 @@ int backend_main(int argc, char **argv) {
                 }
                 continue;
             }
-            server_ch->notify();
         } catch (std::exception &ex) {
             std::cerr << __func__ << " " << __LINE__ << ": exiting \"" << ex.what() << "\"" << std::endl;
             rv = -1; goto finish;
@@ -125,9 +124,11 @@ int backend_main(int argc, char **argv) {
             std::cerr << "unsurpported message" << std::endl;
             rv = -1; goto finish;
         }
+        server_ch->notify();
     }
 
  finish:
+    server_ch->notify();
     signal_handler.shutdown();
     return rv;
 }

common/include/ogawayama/common/row_queue.h
@@ -81,10 +81,9 @@ namespace ogawayama::common {
             }
             
             /**
-             * @brief pop and clear the current row.
+             * @brief pop the current row.
              */
             ogawayama::stub::ErrorCode pop() {
-                get_current_row().clear();
                 if(!is_not_empty()) {
                     boost::interprocess::scoped_lock lock(m_mutex_);
                     while (true) {
@@ -129,6 +128,18 @@ namespace ogawayama::common {
                     lock.unlock();
                 }
                 return m_container_.at(index(pushed_));
+            }
+
+            void resize_writing_row(std::size_t n) {
+                get_writing_row().resize(n);
+            }
+
+            void clear()
+            {
+                m_container_.clear();
+                m_container_.resize(capacity_, ShmRowArgs(allocator_));
+                pushed_ = poped_ = 0;
+                m_types_.clear();
             }
 
             void push_type(ogawayama::stub::Metadata::ColumnType::Type type, std::size_t length) {
@@ -205,9 +216,7 @@ namespace ogawayama::common {
             if (owner_) {
                 shared_memory_->get_managed_shared_memory_ptr()->destroy<SpscQueue>(name_);
             } else {
-                if (shared_memory_->get_managed_shared_memory_ptr()->find<SpscQueue>(name_).first != nullptr) {
-                    queue_->bye();
-                }
+                queue_->bye();
             }
         }
 
@@ -216,9 +225,13 @@ namespace ogawayama::common {
          */
         template<typename T>
         ogawayama::stub::ErrorCode put_next_column(T v) {
-            ShmClmArg column = v;
             try {
-                queue_->get_writing_row().emplace_back(column);
+                auto& row = queue_->get_writing_row();
+                if (row.size() > cindex_) {
+                    row.at(cindex_) = v;
+                } else {
+                    row.emplace_back(v);
+                }
             }
             catch(const SharedMemoryException& ex) {
                 return ogawayama::stub::ErrorCode::TIMEOUT;
@@ -232,15 +245,22 @@ namespace ogawayama::common {
         ogawayama::stub::ErrorCode put_next_column(std::string_view v) {
             ShmString column_string(shared_memory_->get_managed_shared_memory_ptr()->get_segment_manager());
             column_string.assign(v.begin(), v.end());
-            ShmClmArg column = column_string;
             try {
-                queue_->get_writing_row().emplace_back(column);
+                auto& row = queue_->get_writing_row();
+                if (row.size() > cindex_) {
+                    row.at(cindex_) = column_string;
+                } else {
+                    row.emplace_back(column_string);
+                }
             }
             catch(const SharedMemoryException& ex) {
                 return ogawayama::stub::ErrorCode::TIMEOUT;
             }
             cindex_++;
             return ogawayama::stub::ErrorCode::OK;
+        }
+        void resize_writing_row(std::size_t n) {
+            queue_->get_writing_row().resize(n);
         }
         /**
          * @brief push the row into the queue.
@@ -250,6 +270,7 @@ namespace ogawayama::common {
                 auto retv = queue_->push();
                 if (retv != ogawayama::stub::ErrorCode::TIMEOUT) {
                     cindex_ = 0;
+                    queue_->get_writing_row().clear();
                     return retv;
                 }
                 if (!is_alive()) {
@@ -324,6 +345,13 @@ namespace ogawayama::common {
             return true;
         }
         std::size_t get_requested() { return queue_->get_requested(); }
+
+        /**
+         * @brief initialize the SpscQueue, for reuse.
+         */
+        void clear() {
+            queue_->clear();
+        }
 
     private:
         SharedMemory *shared_memory_;

common/include/ogawayama/common/shared_memory.h
@@ -30,8 +30,8 @@
 namespace ogawayama::common {
 
 namespace param {
-    static constexpr std::size_t SEGMENT_SIZE = 100<<20; // 100 MiB (tantative)
-    static constexpr std::size_t MAX_NAME_LENGTH = 32;   // 64 chars (tantative, but probably enough)
+    static constexpr std::size_t SEGMENT_SIZE = 1<<16;   // 64 KB (for channel stream, tantative)
+    static constexpr std::size_t MAX_NAME_LENGTH = 64;   // 64 chars (tantative, but probably enough)
     static constexpr std::size_t QUEUE_SIZE = 32;        // 32 rows (tantative) must be greater than or equal to 2
     static constexpr long TIMEOUT = 10000;   	     // timeout for condition, currentry set to 10 seconds
 
@@ -76,13 +76,13 @@ public:
     /**
      * @brief Construct a new object.
      */
-     SharedMemory(std::string_view database_name, bool create_shm, bool remove_shm = true) : database_name_(database_name), owner_(create_shm)  {
+    SharedMemory(std::string_view database_name, bool create_shm, bool remove_shm = true, std::size_t magnification = 1) : database_name_(database_name), owner_(create_shm)  {
         if (create_shm) {
             if (remove_shm) {
                 boost::interprocess::shared_memory_object::remove(database_name_.c_str());
             }
             try {
-                managed_shared_memory_ = std::make_unique<boost::interprocess::managed_shared_memory>(boost::interprocess::create_only, database_name_.c_str(), param::SEGMENT_SIZE);
+                managed_shared_memory_ = std::make_unique<boost::interprocess::managed_shared_memory>(boost::interprocess::create_only, database_name_.c_str(), param::SEGMENT_SIZE * magnification);
             }
             catch(const boost::interprocess::interprocess_exception& ex) {
                 throw SharedMemoryException("shared memory already exist");

server/src/worker.cpp
@@ -72,7 +72,7 @@ void Worker::run()
             }
             transaction_->commit();
             channel_->send_ack(ERROR_CODE::OK);
-            clear();
+            clear_transaction();
             break;
         case ogawayama::common::CommandMessage::Type::ROLLBACK:
             if (!transaction_) {
@@ -80,7 +80,7 @@ void Worker::run()
             }
             transaction_->abort();
             channel_->send_ack(ERROR_CODE::OK);
-            clear();
+            clear_transaction();
             break;
         case ogawayama::common::CommandMessage::Type::PREPARE:
             prepare(string, ivalue);
@@ -100,7 +100,7 @@ void Worker::run()
             if (transaction_) {
                 transaction_->abort();
             }
-            clear();
+            clear_all();
             channel_->bye_and_notify();
             return;
         default:
@@ -186,6 +186,7 @@ bool Worker::execute_query(std::string_view sql, std::size_t rid)
     try {
         cursor.row_queue_ = std::make_unique<ogawayama::common::RowQueue>
             (shm4_row_queue_->shm_name(ogawayama::common::param::resultset, rid).c_str(), shm4_row_queue_.get());
+        cursor.row_queue_->clear();
     } catch (umikongo::Exception& e) {
         std::cerr << e.what() << std::endl;
         channel_->send_ack(ERROR_CODE::UNKNOWN);
@@ -218,6 +219,7 @@ void Worker::next(std::size_t rid)
         for(std::size_t i = 0; i < limit; i++) {
             auto row = cursor.iterator_->next();
             if (row != nullptr) {
+                rq->resize_writing_row(rq->get_metadata_ptr()->get_types().size());
                 for (auto& t: rq->get_metadata_ptr()->get_types()) {
                     std::size_t cindex = rq->get_cindex();
                     if (row->is_null(cindex)) {
@@ -343,6 +345,7 @@ bool Worker::execute_prepared_query(std::size_t sid, std::size_t rid)
     try {
         cursor.row_queue_ = std::make_unique<ogawayama::common::RowQueue>
             (shm4_row_queue_->shm_name(ogawayama::common::param::resultset, rid).c_str(), shm4_row_queue_.get());
+        cursor.row_queue_->clear();
     } catch (umikongo::Exception& e) {
         std::cerr << e.what() << std::endl;
         channel_->send_ack(ERROR_CODE::UNKNOWN);
@@ -365,12 +368,6 @@ bool Worker::execute_prepared_query(std::size_t sid, std::size_t rid)
         return false;
     }
     return true;
-}
-
-void Worker::clear()
-{
-    cursors_.clear();
-    transaction_ = nullptr;
 }
 
 }  // ogawayama::server

server/src/worker.h
@@ -43,8 +43,7 @@ class Worker {
  public:
     Worker(umikongo::Database *, ogawayama::common::SharedMemory *, std::size_t);
     ~Worker() {
-        clear();
-        prepared_statements_.clear();
+        clear_all();
         if(thread_.joinable()) thread_.join();
     }
     void run();
@@ -79,7 +78,15 @@ class Worker {
 
     void send_metadata(std::size_t);
     void set_params(umikongo::PreparedStatement::Parameters *);
-    void clear();
+    void clear_transaction() {
+        cursors_.clear();
+        transaction_ = nullptr;
+    }
+    void clear_all() {
+        clear_transaction();
+        shm4_row_queue_ = nullptr;
+        prepared_statements_.clear();
+    }
 };
 
 }  // ogawayama::server

server/tools/main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
         ConnectionPtr connection;
         ERROR_CODE err;
 
-        err = stub->get_connection(12, connection);
+        err = stub->get_connection(connection, 12);
         if (err != ERROR_CODE::OK) { prt_err(__LINE__, err); return 1; }
         TransactionPtr transaction;
         err = connection->begin(transaction);
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
         ConnectionPtr connection;
         ERROR_CODE err;
 
-        err = stub->get_connection(12, connection);
+        err = stub->get_connection(connection, 12);
         if (err != ERROR_CODE::OK) { prt_err(__LINE__, err); return 1; }
         TransactionPtr transaction;
         

stub/src/connection.cpp
@@ -26,7 +26,9 @@ Connection::Impl::Impl(Connection *connection, std::size_t pgprocno) : envelope_
         (managed_shared_memory->shm_name(ogawayama::common::param::channel, pgprocno_).c_str(), managed_shared_memory, true);
     parameters_ = std::make_unique<ogawayama::common::ParameterSet>
         (managed_shared_memory->shm_name(ogawayama::common::param::prepared, pgprocno_).c_str(), managed_shared_memory, true);
-    shm4_row_queue_ = std::make_unique<ogawayama::common::SharedMemory>(managed_shared_memory->shm4_row_queue_name(pgprocno), true, true);
+    shm4_row_queue_ = std::make_unique<ogawayama::common::SharedMemory>(managed_shared_memory->shm4_row_queue_name(pgprocno), true, true, 16);
+
+    result_sets_ = std::make_unique<std::vector<std::shared_ptr<ResultSet>>>();
 }
 
 Connection::Impl::~Impl()

stub/src/connectionImpl.h
@@ -45,6 +45,10 @@ public:
     ogawayama::common::ParameterSet * get_parameters() { return parameters_.get(); }
 
     auto get_shm4_row_queue() const { return shm4_row_queue_.get(); }
+    void get_result_sets(std::vector<std::shared_ptr<ResultSet>> * & result_sets) const
+    {
+        result_sets = result_sets_.get();
+    }
 
 private:
     Connection *envelope_;
@@ -55,6 +59,7 @@ private:
     std::size_t sid_{};
 
     std::unique_ptr<ogawayama::common::SharedMemory> shm4_row_queue_;
+    std::unique_ptr<std::vector<std::shared_ptr<ResultSet>>> result_sets_;
 };
 
 }  // namespace ogawayama::stub

commit 9b7aaf47464849d05b2c8f134f99796dfc1a0d2b

Fix a deadlock trouble introduced by the commit d63cbf62ae0d6840892442c671aedc2eb55a31b5

server/src/backend.cpp
@@ -84,7 +84,8 @@ int backend_main(int argc, char **argv) {
             }
         } catch (std::exception &ex) {
             std::cerr << __func__ << " " << __LINE__ << ": exiting \"" << ex.what() << "\"" << std::endl;
-            rv = -1; goto finish;
+            rv = -1;
+            goto finish;
         }
 
         switch (type) {
@@ -120,18 +121,20 @@ int backend_main(int argc, char **argv) {
             }
             break;
         case ogawayama::common::CommandMessage::Type::TERMINATE:
+            server_ch->notify();
             server_ch->lock();
             server_ch->unlock();
             goto finish;
         default:
             std::cerr << "unsurpported message" << std::endl;
-            rv = -1; goto finish;
+            rv = -1;
+            server_ch->notify();
+            goto finish;
         }
         server_ch->notify();
     }
 
  finish:
-    server_ch->notify();
     signal_handler.shutdown();
     return rv;
 }