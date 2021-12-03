1800b29f7ae87016d23481e4675923ea6c7bd69a
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-12-03 10:01:31 +0900

    added suffix to database name to avoid using the same name of shared memory as used by tateyama

--- stub/src/stub.cpp
+++ stub/src/stub.cpp
@@ -26,8 +26,9 @@ Stub::Impl::Impl(Stub *stub, std::string_view database_name) : envelope_(stub)
     //    TODO: implements error handling
     //    if(length > ogawayama::common::param::MAX_DB_NAME_LENGTH) {
     //    }
-    strncpy(name_, database_name.data(), length);
-    shared_memory_ = std::make_unique<ogawayama::common::SharedMemory>(database_name, ogawayama::common::param::SheredMemoryType::SHARED_MEMORY_SERVER_CHANNEL);
+    name_ = database_name;
+    name_ += "-o";
+    shared_memory_ = std::make_unique<ogawayama::common::SharedMemory>(name_, ogawayama::common::param::SheredMemoryType::SHARED_MEMORY_SERVER_CHANNEL);
     server_ = std::make_unique<ogawayama::common::ChannelStream>(ogawayama::common::param::server, shared_memory_.get());
 }
 
