c06386fb4f8ca16fbae245095ff3e0dc0a54b0a8
t-horikawa <horikawa@nautilus-technologies.com>
2025-04-16 09:05:16 +0900

    update sql/[common|response].proto to be the same as jogasaki and update SMV

--- src/ogawayama/transport/transport.h
+++ src/ogawayama/transport/transport.h
@@ -48,7 +48,7 @@ class transport {
     constexpr static std::size_t CORE_MESSAGE_VERSION_MAJOR = 0;
     constexpr static std::size_t CORE_MESSAGE_VERSION_MINOR = 0;
     constexpr static std::size_t SQL_MESSAGE_VERSION_MAJOR = 1;
-    constexpr static std::size_t SQL_MESSAGE_VERSION_MINOR = 4;
+    constexpr static std::size_t SQL_MESSAGE_VERSION_MINOR = 5;
     constexpr static std::size_t ENDPOINT_MESSAGE_VERSION_MAJOR = 0;
     constexpr static std::size_t ENDPOINT_MESSAGE_VERSION_MINOR = 0;
     constexpr static std::uint32_t SERVICE_ID_ROUTING = 0;  // from tateyama/framework/component_ids.h
