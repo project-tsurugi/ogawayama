6a4b77dd50438862311b90a579c24d3aa785dcef
t-horikawa <horikawa@nautilus-technologies.com>
2025-11-18 16:54:30 +0900

    BREAKING CHANGE: add reference tag for BLOB/CLOB result set fields and common.LargeObjectReference, and SMV major update

--- src/ogawayama/transport/transport.h
+++ src/ogawayama/transport/transport.h
@@ -47,8 +47,8 @@ class transport {
     constexpr static std::size_t HEADER_MESSAGE_VERSION_MINOR = 1;
     constexpr static std::size_t CORE_MESSAGE_VERSION_MAJOR = 0;
     constexpr static std::size_t CORE_MESSAGE_VERSION_MINOR = 0;
-    constexpr static std::size_t SQL_MESSAGE_VERSION_MAJOR = 1;
-    constexpr static std::size_t SQL_MESSAGE_VERSION_MINOR = 6;
+    constexpr static std::size_t SQL_MESSAGE_VERSION_MAJOR = 2;
+    constexpr static std::size_t SQL_MESSAGE_VERSION_MINOR = 0;
     constexpr static std::size_t ENDPOINT_MESSAGE_VERSION_MAJOR = 0;
     constexpr static std::size_t ENDPOINT_MESSAGE_VERSION_MINOR = 0;
     constexpr static std::uint32_t SERVICE_ID_ROUTING = 0;  // from tateyama/framework/component_ids.h
